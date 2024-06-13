c9e495a6544c69afb17ac99e92ab455ed8f0bed5
mubashir <mubashirkilayil8@gmail.com>
2024-06-13 13:50:00 +0530

    calculator source code

---
+++ script.js
@@ -0,0 +1,20 @@
+// function readVal(val){
+//     document.querySelector("#result").value+=val
+// }
+// function clean(){
+//     document.querySelector("#result").value="0"
+// }
+// function cleanOne() {
+//     let back=document.querySelector("#result").value
+//     let result=back.slice(0,-1)
+//     document.querySelector("#result").value=result
+//   }
+// function showResult(){
+//     var text=document.querySelector("#result").value
+//     var result= eval(text)
+//     document.querySelector("#result").value=result
+// }
+var readVal=(val)=>result.value+=val
+var clean=()=>result.value="0"
+var cleanOne=()=>result.value=result.value.slice(0,-1)
+var showResult=()=>result.value=eval(result.value)
